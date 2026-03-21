8ccda15f63c61fea76c3f9000bbba3edda1b62cc
Dimi Kot <dimi.kot.code@gmail.com>
2026-03-21 10:44:54 -0700

    Implement greeting feature with button click in Home component

---
+++ src/app/actions.ts
@@ -0,0 +1,10 @@
+"use server";
+
+export async function getGreeting() {
+  const values = [3.14, 6.28, 9.42, 12.56];
+  const message = ["numbers:"];
+  for (let i = 0; i <= values.length; i++) {
+    message.push(String(values[i].toFixed(1)));
+  }
+  return message.join(" ");
+}
